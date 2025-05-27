b43fee61a94985832ca09483a5139cc22637f4b3
Jung-gyucheol <wjdrbcjf2489@gmail.com>
2025-05-27 09:33:59 +0900

    fix: crawler.js

--- src/app/api/naver/crawler.js
+++ src/app/api/naver/crawler.js
@@ -27,7 +27,7 @@ const the_climbs = [
 
 async function crawlNaverData() {
   try {
-    const browser = await puppeteer.launch({
+    const browser1 = await puppeteer.launch({
       headless: 'new',
       args: [
         '--no-sandbox',
@@ -37,15 +37,15 @@ async function crawlNaverData() {
       ],
       protocolTimeout: 1000 * 60 * 10,
     })
-    const page = await browser.newPage()
-    await page.setUserAgent(
+    const page1 = await browser1.newPage()
+    await page1.setUserAgent(
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
     )
 
     const times = []
-    for (const theClimb of the_climbs) {
+    for (const theClimb of the_climbs.slice(0, 8)) {
       console.log('init', theClimb.ko)
-      await page.goto(`https://map.naver.com/p/search/더클라임${theClimb.ko}`)
+      await page1.goto(`https://map.naver.com/p/search/더클라임${theClimb.ko}`)
       await new Promise((resolve) => setTimeout(resolve, 5000))
 
       console.log('searchFrame', theClimb.ko)
@@ -65,13 +65,95 @@ async function crawlNaverData() {
           }
         }, theClimb)
         await new Promise((resolve) => setTimeout(resolve, 5000))
-        const url = page.url()
+        const url = page1.url()
 
         console.log('url', url)
-        await page.goto(url)
+        await page1.goto(url)
         await new Promise((resolve) => setTimeout(resolve, 5000))
       }
-      const EntryIFrame = await page.$('#entryIframe')
+      const EntryIFrame = await page1.$('#entryIframe')
+      const entryFrame = await EntryIFrame?.contentFrame()
+      console.log('entryFrame', theClimb.ko)
+
+      if (!entryFrame) {
+        throw new Error('entryFrame not found')
+      }
+
+      console.log('climbingContent', theClimb.ko)
+      const climbingContent = await entryFrame.evaluate(async () => {
+        const content = document.querySelectorAll('em')
+        const address = document.querySelectorAll(
+          'div[class="place_section_content"] a',
+        )
+        console.log(address[0].innerText)
+        content[0].click()
+        await new Promise((resolve) => setTimeout(resolve, 2000))
+        const parentElement =
+          content[0].parentElement?.parentElement?.parentElement
+        const firstClass = parentElement?.classList[0]
+        const children = document.querySelectorAll(`div[class="${firstClass}"]`)
+        return {
+          address: address[0].innerText,
+          business_hours: Array.from(children).map((item) =>
+            item.innerText.split('\n').splice(0, 2),
+          ),
+        }
+      })
+      // console.log('success!!', theClimb.ko)
+      // await supabase
+      //   .from('climbing_branch')
+      //   .update({
+      //     business_hours: climbingContent.business_hours,
+      //   })
+      //   .eq('branch', theClimb.ko)
+      // console.log('success!', theClimb.ko)
+      times.push(climbingContent)
+      await browser1.close()
+    }
+
+    const browser2 = await puppeteer.launch({
+      headless: 'new',
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-gpu',
+      ],
+      protocolTimeout: 1000 * 60 * 10,
+    })
+    const page2 = await browser2.newPage()
+    await page2.setUserAgent(
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    )
+    for (const theClimb of the_climbs.slice(8)) {
+      console.log('init', theClimb.ko)
+      await page2.goto(`https://map.naver.com/p/search/더클라임${theClimb.ko}`)
+      await new Promise((resolve) => setTimeout(resolve, 5000))
+
+      console.log('searchFrame', theClimb.ko)
+      const searchFrame = await page2.$('#searchIframe')
+      console.log('searchFrame', searchFrame)
+      const frame = await searchFrame?.contentFrame()
+      console.log('frame', frame)
+      if (frame) {
+        console.log('inframe', theClimb.ko)
+        await frame.evaluate(async (theClimb) => {
+          const content = document.querySelectorAll('ul span')
+          for (const span of content) {
+            if (span.textContent?.includes(theClimb.ko)) {
+              span.click()
+              return
+            }
+          }
+        }, theClimb)
+        await new Promise((resolve) => setTimeout(resolve, 5000))
+        const url = page2.url()
+
+        console.log('url', url)
+        await page2.goto(url)
+        await new Promise((resolve) => setTimeout(resolve, 5000))
+      }
+      const EntryIFrame = await page2.$('#entryIframe')
       const entryFrame = await EntryIFrame?.contentFrame()
       console.log('entryFrame', theClimb.ko)
 
@@ -110,6 +192,7 @@ async function crawlNaverData() {
       times.push(climbingContent)
     }
     console.log(times)
+    await browser2.close()
     // await new Promise((resolve) => setTimeout(resolve, 3000))
     // await page.evaluate(() => {
     //   const content = document.querySelectorAll('span')
@@ -117,7 +200,6 @@ async function crawlNaverData() {
     // })
 
     // console.log(content)
-    await browser.close()
   } catch (error) {
     console.error(error)
   }
